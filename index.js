ccb00bb94826aa4d7577d3902d37d0db5502ff0c
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-09-01 08:05:20 -0700

    made a main div in html file, made a function to make 256 smaller divs inside of the main div in javascript, and added some css to make the smaller divs squares and aligned them in a 16x16 grid.

---
+++ index.js
@@ -0,0 +1,8 @@
+for(x=0; x<256; x++) {
+    const contentDivs = document.createElement('div');
+    contentDivs.className = 'contentDivs';
+
+    document.getElementById('mainContainer').appendChild(contentDivs);
+}
+
+
