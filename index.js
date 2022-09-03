28b5ef3bc533b73e0db78c52f1745f65dff8f8e2
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-09-03 16:22:11 -0700

    each div now turns black on hover using the mouseenter eventlistener

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-for(x=0; x<256; x++) {
+// changed x from 256 to 16 so the divs could fit on screen. switch x back to 256 when done
+for(x=0; x<64; x++) {
     const contentDivs = document.createElement('div');
-    contentDivs.className = 'contentDivs';
+    contentDivs.id = 'contentDivs';
 
     document.getElementById('mainContainer').appendChild(contentDivs);
 }
 
-
+document.querySelectorAll('#contentDivs').forEach(function(element) {
+    element.addEventListener('mouseenter', () => {
+        element.style.background = 'black';
+    })
+})
