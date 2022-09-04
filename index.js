be58a1d7b84cc14e0934905cfed222c68e13616b
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-09-03 18:17:38 -0700

    added h1 and footer

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // changed x from 256 to 16 so the divs could fit on screen. switch x back to 256 when done
-for(x=0; x<64; x++) {
+for(x=0; x<256; x++) {
     const contentDivs = document.createElement('div');
-    contentDivs.id = 'contentDivs';
+    contentDivs.className = 'contentDivs';
 
-    document.getElementById('mainContainer').appendChild(contentDivs);
+    document.getElementById('contentDivContainer').appendChild(contentDivs);
 }
 
-document.querySelectorAll('#contentDivs').forEach(function(element) {
-    element.addEventListener('mouseenter', () => {
+document.querySelectorAll('.contentDivs').forEach(function(element) {
+    element.addEventListener('mouseover', () => {
         element.style.background = 'black';
     })
 })
