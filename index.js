1e122275a2e884b412f6d8f2086d967477542644
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-09-25 19:17:59 -0700

    Added clear button and changed ID names to more informational names.
    html changes:
    - added clear button
    - changed id of 'boardContainer' to 'gridContainer'
    - changed id of 'contentDivContainer' to 'grid'
    
    css changes:
    - changed 'contentDivContainer' to 'grid'
    
    javascript changes:
    - added variable declarations/querySelector's for densityButton and clearBoard
    - changed 'contentDivContainer' to 'grid'
    - added click eventListeners for densityButton and clearBoard buttons

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// variable declarations
+const densityButton = document.querySelector('#densityButton');
+const clearBoard = document.querySelector('#clearBoard');
+
 // changed x from 256 to 16 so the divs could fit on screen. switch x back to 256 when done
 for(x=0; x<256; x++) {
     const contentDivs = document.createElement('div');
     contentDivs.className = 'contentDivs';
 
-    document.getElementById('contentDivContainer').appendChild(contentDivs);
+    document.getElementById('grid').appendChild(contentDivs);
 }
 
 document.querySelectorAll('.contentDivs').forEach(function(element) {
@@ -11,3 +15,12 @@ document.querySelectorAll('.contentDivs').forEach(function(element) {
         element.style.background = 'black';
     })
 })
+
+// event listeners
+densityButton.addEventListener('click', () => {
+    prompt('Enter number of squares per side (max: 100)');
+});
+
+clearBoard.addEventListener('click', () => {
+
+});
