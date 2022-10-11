52ea68081caec2c8143305855e6ee7ca55a6b89a
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-10-11 00:27:15 -0700

    Cleaned up javascript file and added functions
    - Moved mouseover eventlistener into a function called colorPixel().
    - Assigned prompt to a variable called boardDensity
    - Added a for loop inside of densityButton eventlistener/prompt to generate
    a new board.

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 // variable declarations
 const densityButton = document.querySelector('#densityButton');
 const clearBoard = document.querySelector('#clearBoard');
+let boardDensity;
 
-// changed x from 256 to 16 so the divs could fit on screen. switch x back to 256 when done
+// creates initial grid upon page load
 for(x=0; x<256; x++) {
     const contentDivs = document.createElement('div');
     contentDivs.className = 'contentDivs';
 
     document.getElementById('grid').appendChild(contentDivs);
+    colorPixel();
 }
 
-document.querySelectorAll('.contentDivs').forEach(function(element) {
-    element.addEventListener('mouseover', () => {
+function removeAllContentDivs(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// turns contentDiv's black upon mouseover
+function colorPixel() {
+    document.querySelectorAll('.contentDivs').forEach(function(element) {
+        element.addEventListener('mouseover', () => {
         element.style.background = 'black';
+        })
     })
-})
+    document.querySelectorAll('.horizontalDivs').forEach(function(element) {
+        element.addEventListener('mouseover', () => {
+        element.style.background = 'black';
+        })
+    })
+}
+
+
 
 // event listeners
 densityButton.addEventListener('click', () => {
-    prompt('Enter number of squares per side (max: 100)');
+    let boardDensity = prompt('Enter number of squares per side (max: 100)');
+    removeAllContentDivs(grid);
+    grid.setAttribute('style', `grid-template-columns:repeat(${boardDensity}, 2fr); grid-template-rows:repeat(${boardDensity}, 2fr)`);
+
+    for(i = 0; i < boardDensity*boardDensity; i++) {
+        const horizontalDivs = document.createElement('div');
+        horizontalDivs.className = 'horizontalDivs';
+        document.getElementById('grid').appendChild(horizontalDivs);
+        colorPixel();
+    }
 });
 
 clearBoard.addEventListener('click', () => {
