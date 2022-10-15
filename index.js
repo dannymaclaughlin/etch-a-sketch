f03d37aa3dc01917e0282f773ba603b84d3c4460
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-10-15 14:41:24 -0700

    Cleaned up index.js file by creating functions.
    - Made function initGrid() to house code that initialized grid upon page load.
    - Moved code from densityButton eventlistener to new function generateNewGrid().
    - Renamed removeAllContentDivs function to removeGrid as that is a better description of function.

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const clearBoard = document.querySelector('#clearBoard');
 let boardDensity;
 
 // creates initial grid upon page load
-for(x=0; x<256; x++) {
-    const contentDivs = document.createElement('div');
-    contentDivs.className = 'contentDivs';
-
-    document.getElementById('grid').appendChild(contentDivs);
-    colorPixel();
+function initGrid() {
+    for(x=0; x<256; x++) {
+        const contentDivs = document.createElement('div');
+        contentDivs.className = 'contentDivs';
+        document.getElementById('grid').appendChild(contentDivs);
+        
+        colorPixel();
+    }  
 }
 
-function removeAllContentDivs(parent) {
+// generates new grid based off of value given in prompt
+function generateNewGrid() {
+    let boardDensity = prompt('Enter number of squares per side (max: 25)');
+    removeGrid(grid);
+    grid.setAttribute('style', `grid-template-columns:repeat(${boardDensity}, 2fr); grid-template-rows:repeat(${boardDensity}, 2fr)`);
+
+    for(i = 0; i < boardDensity*boardDensity; i++) {
+        const horizontalDivs = document.createElement('div');
+        horizontalDivs.className = 'horizontalDivs';
+        document.getElementById('grid').appendChild(horizontalDivs);
+        colorPixel();
+    }
+}
+
+// function to remove grid
+function removeGrid(parent) {
     while(parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
@@ -32,22 +49,13 @@ function colorPixel() {
     })
 }
 
-
-
 // event listeners
 densityButton.addEventListener('click', () => {
-    let boardDensity = prompt('Enter number of squares per side (max: 100)');
-    removeAllContentDivs(grid);
-    grid.setAttribute('style', `grid-template-columns:repeat(${boardDensity}, 2fr); grid-template-rows:repeat(${boardDensity}, 2fr)`);
-
-    for(i = 0; i < boardDensity*boardDensity; i++) {
-        const horizontalDivs = document.createElement('div');
-        horizontalDivs.className = 'horizontalDivs';
-        document.getElementById('grid').appendChild(horizontalDivs);
-        colorPixel();
-    }
+    generateNewGrid();
 });
 
 clearBoard.addEventListener('click', () => {
 
 });
+
+initGrid();
